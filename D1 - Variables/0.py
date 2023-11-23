41ec81747e7ce4d30825811fe3c2c291e8e8fddf
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-24 01:41:37 +0530

    add 1 string manipulation

--- D1 - Variables/0.py
+++
@@ -1,1 +0,0 @@
-print("Hello Abdul Rahman!")
---
+++ D1 - Variables/1 String Manipulation.py
@@ -0,0 +1,14 @@
+# Strinh Manipulation 
+
+# \n - new line
+print("hello world!\nhello world!")
+
+# Concatenation
+print("hello" + " " +"Abdul Rahman")
+
+# Defining strings 
+var1 = "Hello "
+var2 = "Abdul"
+# + Operator is used to combine strings
+var3 = var1 + var2
+print(var3)
