d8afeebb8a9f815c9c2ea1b568d82bd6e17b4edf
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-24 01:54:15 +0530

    add 2 input fundtion file

---
+++ D1 - Variables/2 Input Function.py
@@ -0,0 +1,7 @@
+# the python input function
+
+# input("A prompy for the user")
+
+print("hello " + input("What is your name?"))
+# input() will get user input in console
+# Then print () will print the word "hello" and the user input
