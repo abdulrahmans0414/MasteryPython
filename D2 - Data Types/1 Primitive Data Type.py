aea576567ad6c6560b0fe3c46bbaeb462802adbc
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-25 00:41:04 +0530

    add primitive data types

---
+++ D2 - Data Types/1 Primitive Data Type.py
@@ -0,0 +1,23 @@
+# Data Types
+
+# String 
+print("Hello"[0])
+
+print("123" +"345")
+print("hello" +"world")
+
+
+# Integer
+
+print(123+123)
+
+123_456_123
+
+# Float
+
+3.14159
+
+# Boolean
+
+True
+False
