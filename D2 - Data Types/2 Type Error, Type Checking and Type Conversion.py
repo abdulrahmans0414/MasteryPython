7dee7527d5a9f0e05e313586edf83a809b9cb282
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-25 00:56:19 +0530

    add type error ,type checking and type conversion

---
+++ D2 - Data Types/2 Type Error, Type Checking and Type Conversion.py
@@ -0,0 +1,23 @@
+# Type error , type checking and type conversion
+
+# num_char = len(input("What is your name?"))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
+
+# print(type(num_char))
+
+
+# type check
+a = 123
+print(type(a))
+
+
+# type conversion
+a=str(123)
+print(type(a))
+
+a=float(123)
+print(type(a))
+
+# print(70 + float("100.5"))
+print(str(70)+ str(100))
