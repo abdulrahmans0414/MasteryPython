1e3caafb2eb88618c799f78e711466d88562f4ea
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-25 23:50:09 +0530

    add mathmatical Operations

---
+++ D2 - Data Types/3 Mathematical Operations.py
@@ -0,0 +1,14 @@
+# Mathmatical Operations in Python
+# 3 + 5 addition
+# 7 - 4 subtraction
+# 3 * 2 multiplication
+# 6 / 3 division
+# print(type(6/3))  float 
+# ** power 
+# print(2**3)
+
+
+# PEMDAS 
+# (parenthese > Exponent > Mult > Div > Add > Sub)
+
+print(3 * 3 + 3 / 3 -3 )
