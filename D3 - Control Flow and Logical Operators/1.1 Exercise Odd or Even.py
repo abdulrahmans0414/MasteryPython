a548bb585b70e1069961f5f822a956c2ac78baba
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-29 05:08:46 +0530

    add exercise Odd or Even number practice

---
+++ D3 - Control Flow and Logical Operators/1.1 Exercise Odd or Even.py
@@ -0,0 +1,12 @@
+# Check the number is odd or even
+
+# Which number you want to check?
+number =int(input("Write any random number"))
+
+# if the number can be divided by 2 with o remainder.
+if number % 2 == 0:
+    print("This is an even number .")
+
+# otherwise (number cannot be divided by 2 with o remainder).
+else:
+    print("This is an odd number .")
