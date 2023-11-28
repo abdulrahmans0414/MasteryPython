8690ef79311534486e7e4c9c48129d8ec4f2ff80
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-29 04:52:20 +0530

    add if else and condition operator

---
+++ D3 - Control Flow and Logical Operators/1 if else and condition operators.py
@@ -0,0 +1,15 @@
+# : colon and indent that is block of code 
+
+print("Welcome to the Indian Army !")
+height = int(input("What is your height in cm ? "))
+
+# condition 
+if height >= 170:
+    print("You are eligible to join the Indian Army.")
+else:
+    print(" Sorry, You are not eligible to join the Indian Army .")
+    
+
+# = it mean that you're assiging this value to this value
+# == it means that you are checking to see if the value on the left is equal to the value on the right
+# === it means you are also cheching datatype
