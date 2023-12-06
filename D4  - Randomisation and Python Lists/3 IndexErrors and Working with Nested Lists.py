b21622b9a03edac0a662abe35fef936bad94ef49
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-06 22:37:17 +0530

    add IndexErrors and Working with Nested list

---
+++ D4  - Randomisation and Python Lists/3 IndexErrors and Working with Nested Lists.py
@@ -0,0 +1,41 @@
+# Undestanding the Offset and Appending items to lists
+
+indian_states = [
+    'Andhra Pradesh',
+    'Arunachal Pradesh',
+    'Assam',
+    'Bihar',
+    'Chhattisgarh',
+    'Goa',
+    'Gujarat',
+    'Haryana',
+    'Himachal Pradesh',
+    'Jharkhand',
+    'Karnataka',
+    'Kerala',
+    'Madhya Pradesh',
+    'Maharashtra',
+    'Manipur',
+    'Meghalaya',
+    'Mizoram',
+    'Nagaland',
+    'Odisha',
+    'Punjab',
+    'Rajasthan',
+    'Sikkim',
+    'Tamil Nadu',
+    'Telangana',
+    'Tripura',
+    'Uttar Pradesh',
+    'Uttarakhand',
+    'West Bengal',
+    'Andaman and Nicobar Islands',
+    'Chandigarh',
+    'Dadra and Nagar Haveli and Daman and Diu',
+    'Delhi',
+    'Lakshadweep',
+    'Puducherry'
+]
+print(len(indian_states))
+
+dozen =["staraway", "Spinach", "kale", "Nactarines", "Apples", "grapes", "Peaches", "Cherries", "Pears"]
