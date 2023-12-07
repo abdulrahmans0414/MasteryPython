60b92a63f29e5b3723cc03b4e2288e07a3c9cdf9
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-07 13:57:41 +0530

    complete exercise treasure map.py

--- D4  - Randomisation and Python Lists/3.1 Exercise Treaassure Map.py
+++ D4  - Randomisation and Python Lists/3.1 Exercise Treaassure Map.py
@@ -76,10 +76,12 @@ line3 = ["⬜️️","⬜️️","⬜️️"]
 map = [line1, line2, line3]
 print("Hiding your treasure! X marks the spot.")
 position = input() # Where do you want to put the treasure?
-# 🚨 Don't change the code above 👆
-# Write your code below this row 👇
 
+letter = position[0].lower()
+abc = ["a", "b", "c"]
+letter_index = abc.index(letter)
+number_index = int(position[1]) - 1
+map[number_index][letter_index] = "X"
 
-# Write your code above this row 👆
-# 🚨 Don't change the code below 👇
 print(f"{line1}\n{line2}\n{line3}")
+
