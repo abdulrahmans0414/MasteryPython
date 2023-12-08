702c92f5f56140e760edf97f94725290b65bb9a9
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-08 22:05:39 +0530

    add logic to rock paper

--- D4  - Randomisation and Python Lists/p4 Rock Paper.py
+++ D4  - Randomisation and Python Lists/p4 Rock Paper.py
@@ -1,5 +1,7 @@
 # project Rock Paper
 
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -27,4 +29,23 @@ scissors = '''
 ---.__(___)
 '''
 
-print(rock)
+user_choices = int(input("What do you choose ? Type 0 for Rock , 1 for Paper or 2 for Scissors.\n" ))
+
+computer_choices = random.randint(0,2)
+print (f"Computer choices {computer_choices}")
+
+if user_choices >= 3 or user_choices <0 :
+    print("you type an invalid number, you lose !")
+  
+elif computer_choices == 0 and  computer_choices == 2:
+    print("you wins !")
+  
+elif computer_choices == 1 and user_choices == 2:
+    print("you lose !")
+    
+elif computer_choices > user_choices:
+    print("You lose !")
+elif user_choices > computer_choices:
+    print("You win !")
+elif computer_choices == user_choices:
+    print("It's a draw")
