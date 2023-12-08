124968207e2158e44444e7278f0bdd3ef7e568d0
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-08 22:21:21 +0530

    manage to debug code and figure out the issue

--- D4  - Randomisation and Python Lists/p4 Rock Paper.py
+++ D4  - Randomisation and Python Lists/p4 Rock Paper.py
@@ -32,24 +32,25 @@ scissors = '''
 game_images = [rock, paper, scissors]
 
 user_choices = int(input("What do you choose ? Type 0 for Rock , 1 for Paper or 2 for Scissors.\n" ))
-print(game_images[user_choices])
-
-computer_choices = random.randint(0,2)
-print ("Computer choices :")
-print(game_images[computer_choices])
-
 if user_choices >= 3 or user_choices <0 :
     print("you type an invalid number, you lose !")
-  
-elif computer_choices == 0 and  computer_choices == 2:
-    print("you wins !")
-  
-elif computer_choices == 1 and user_choices == 2:
-    print("you lose !")
+else:
+    print(game_images[user_choices])
+
+    computer_choices = random.randint(0,2)
+    print ("Computer choices :")
+    print(game_images[computer_choices])
+
+
+    if computer_choices == 0 and  computer_choices == 2:
+        print("you wins !")
     
-elif computer_choices > user_choices:
-    print("You lose !")
-elif user_choices > computer_choices:
-    print("You win !")
-elif computer_choices == user_choices:
-    print("It's a draw")
+    elif computer_choices == 1 and user_choices == 2:
+        print("you lose !")
+        
+    elif computer_choices > user_choices:
+        print("You lose !")
+    elif user_choices > computer_choices:
+        print("You win !")
+    elif computer_choices == user_choices:
+        print("It's a draw")
