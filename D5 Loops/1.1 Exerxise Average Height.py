95bdcd0a2b2c7e11c5dc1affdeca7f598fbd9114
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-12 23:05:59 +0530

    complete exercise average hight

--- D5 Loops/1.1 Exerxise Average Height.py
+++ D5 Loops/1.1 Exerxise Average Height.py
@@ -39,5 +39,16 @@ average height = 158
 student_heights = input().split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
-  
-# Write your code below this row 👇
+
+total_height = 0
+for height in student_heights:
+  total_height += height
+print(f"total height = {total_height}")
+
+number_of_students = 0
+for student in student_heights:
+  number_of_students += 1
+print(f"number of students = {number_of_students}")
+
+average_height = round(total_height / number_of_students)
+print(f"average height = {average_height}")
