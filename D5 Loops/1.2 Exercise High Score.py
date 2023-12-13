3af9e8d61ef245c4b0719048c330eeedcafd8714
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-13 15:19:29 +0530

    complete exercise high score using for loop

--- D5 Loops/1.2 Exercise High Score.py
+++ D5 Loops/1.2 Exercise High Score.py
@@ -23,3 +23,10 @@ for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 
 # Write your code below 👇
+highest_score = 0
+for score in student_scores:
+  if score > highest_score:
+    highest_score = score
+    # print(highest_score)
+
+print(f"The highest score in the class is: {highest_score}")
