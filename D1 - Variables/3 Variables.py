08c68f14949424e15a64fa23e8ba5a8439829702
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-01-17 11:50:26 +0530

    add variables description

--- D1 - Variables/3 Variables.py
+++ D1 - Variables/3 Variables.py
@@ -1,4 +1,5 @@
 # pyhthon variables
+# Learn to store values in containers for later use. Variables is a concept in programming that allows us to give a label to a piece of data so that we can refer or reference that data using the chosen variable name. We will see in this lesson how to create variables and how to use the variables to access the contained value.
 
 name = "Abdul"
 print(name)
