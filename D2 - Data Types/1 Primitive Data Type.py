dd98b55adcb0c1c606eefdc37c3d38a1a8d146d4
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-01-17 12:18:54 +0530

    update datatype

--- D2 - Data Types/1 Primitive Data Type.py
+++ D2 - Data Types/1 Primitive Data Type.py
@@ -1,23 +1,30 @@
 # Data Types
+"""
+Learn about the primitive data types in Python.
 
-# String 
+Strings
+Integers
+Floats
+Booleans
+"""
+
+# Subscripting
 print("Hello"[0])
 
+# String 
 print("123" +"345")
 print("hello" +"world")
 
 
 # Integer
-
 print(123+123)
 
-123_456_123
+# Large Integers
+print(123_456_123)
 
-# Float
-
-3.14159
+# Float = Floating Point Number
+print(3.14159)
 
 # Boolean
-
-True
-False
+print(True)
+print(False)
