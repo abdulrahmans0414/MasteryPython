0f91ac9a4e0a302d97ed88774e4d1972fbb5ec9d
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-01-17 12:12:24 +0530

    add multiple line comment and also a problem in line17

--- D1 - Variables/3 Variables.py
+++ D1 - Variables/3 Variables.py
@@ -13,3 +13,18 @@ name = input("What is your name?")
 length = len(name)
 print(length)
 
+
+"""
+Variables
+We have 2 variables glass1 and glass2. glass1 contains milk and 
+glass2 contains juice. Write 3 lines of code to switch the contents of the variables. 
+You are not allowed to type the words "milk" or "juice". 
+You are only allowed to use variables to solve this exercise.
+"""
+
+glass1 = "milk"
+glass2 = "juice"
+
+temp = glass1
+glass1 = glass2
+glass2 = temp
